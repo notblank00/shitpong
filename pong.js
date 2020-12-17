585255c95a856456d88e7b8208a200f0a9499a57
notblank00 <pyromancy00@gmail.com>
2020-12-17 18:06:58 +0300

    changed one offensive comment to another and added bleep bloops

--- pong.js
+++ pong.js
@@ -1,5 +1,8 @@
 var canvas = document.getElementById("pongCanvas");   // E
 var ctx = canvas.getContext("2d");
+const bleep = new Audio('bleep.mp3');
+const bloop = new Audio('bloop.mp3');
+bleep.play();
 let rand = Math.random();
 const ballspeed = 15
 var vct = {x : 10, y : -10};
@@ -14,7 +17,7 @@ const reactionSpeed = 20;
 const r = 20;
 var pos = {x : 40, y : 20 + Math.random() * (canvas.height - 40)};
 var key;
-var aiInterval;
+var aiInterval, drawInterval;
 canvas.style.width = window.innerWidth + "px";
 canvas.style.height = window.innerHeight + "px";
 document.addEventListener('keypress', keyPress);
@@ -32,48 +35,68 @@ function keyPress(e) {
         if(ly + len / 2 > 720)
             ly = 720 - len / 2;
     }
+    else if(e.code == "Space") {
+        drawInterval = setInterval(draw, 20);
+        aiInterval = setInterval(ai, reactionSpeed);
+    }
 }
 
 var xtext = canvas.width / 2 - ctx.measureText('Shitpong!').width / 2;
+ctx.font = '24px serif';
+var xsub = canvas.width / 2 - ctx.measureText('Why am I here?').width / 2;
 
 function draw() {
+    ctx.font = '48px serif'
     if(flag == 'lose') {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
         ctx.beginPath();
+        ctx.fillStyle = "#FFFFFF"
         ctx.fillText("Fuck you!", 100, 100);
         ctx.closePath();
+        clearInterval(drawInterval);
     }
     else if(flag == 'win') {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
         ctx.beginPath();
+        ctx.fillStyle = "#FFFFFF"
         ctx.fillText("Fuck me!", 100, 100);
         ctx.closePath();
+        clearInterval(drawInterval);
     }
     else {
         pos.x += vct.x;
         pos.y += vct.y;
         if(pos.x - r < w) {
-            if(ly + len / 2 < pos.y || ly - len / 2 > pos.y)
+            if(ly + len / 2 < pos.y || ly - len / 2 > pos.y) {
                 flag = 'lose';
+                bloop.play();
+            }
             else {
                 vct.x *= -1;
                 aiInterval = setInterval(ai, reactionSpeed);
+                bleep.play();
             }
         }
         if(pos.x + r > canvas.width - w) {
-            if(ry + len / 2 < pos.y || ry - len / 2 > pos.y)
+            if(ry + len / 2 < pos.y || ry - len / 2 > pos.y) {
                 flag = 'win';
+                bloop.play();
+            }
             else {
                 vct.x *= -1;
                 clearInterval(aiInterval);
+                bleep.play();
             }
         }
         if(pos.y - r < 0 || pos.y + r > canvas.height) {
             vct.y *= -1;
         }
+
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.beginPath();
         ctx.fillText("Shitpong!", xtext, 100);
+        ctx.font = '24px serif'
+        ctx.fillText("Why am I here?", xsub, 130);
         ctx.closePath();
         ctx.beginPath();
         ctx.rect(0, ly - len / 2, w, len);
@@ -91,7 +114,6 @@ function draw() {
 }
 
 function ai() {
-    console.log('ai called');
     err = (Math.random() - 0.5) * errFac;
     if(pos.y > ry + len / 2 + err)
         ry += spd;
@@ -99,5 +121,4 @@ function ai() {
         ry -= spd;
 }
 
-setInterval(draw, 20)
-aiInterval = setInterval(ai, reactionSpeed);
+
